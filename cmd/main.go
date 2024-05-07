c4158d924d8e33edbc15220ad16d5eb820dceda5
fatkulnurk <fatkulnurk@gmail.com>
2024-05-07 17:16:59 +0700

    test buat handler & cek method

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
+type Response struct {
+	Message string `json:"message"`
+}
+
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			resp := Response{"Method salah"}
+			_ = json.NewEncoder(w).Encode(resp)
+
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		resp := Response{"oke handler bisa"}
+		_ = json.NewEncoder(w).Encode(resp)
+	})
 	log.Println("Starting server on :8180")
-	http.ListenAndServe(":8180", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
-	}))
+	err := http.ListenAndServe(":8180", mux)
+	log.Fatal(err)
 }
